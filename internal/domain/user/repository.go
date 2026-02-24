f5a9f8db55e5e8bb9992f0cca4dda0cc3919124b
佐藤 <shatang821@gmail.com>
2026-02-24 19:33:38 +0900

    feat: define user repository

--- internal/domain/user/repository.go
+++ internal/domain/user/repository.go
@@ -1,5 +1,13 @@
 package user
 
+import "context"
+
+type UserRepository interface {
+	Save(ctx context.Context, user *User) error
+	FindByEmail(ctx context.Context, email string) (*User, error)
+	FindByID(ctx context.Context, id UserID) (*User, error)
+}
+
 type PassWordHasher interface {
 	Hash(password string) (string, error)
 	Compare(hashedPassword, password string) error
