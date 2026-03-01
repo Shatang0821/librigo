6bc622761725e86a3a7e9de5e440383bce0baabb
佐藤 <shatang821@gmail.com>
2026-03-01 17:48:34 +0900

    feat(backend): add SignIn endpoint for user authentication

--- backend/internal/handler/user_handler.go
+++ backend/internal/handler/user_handler.go
@@ -16,20 +16,31 @@ func NewUserHandler(uc usecase.UserUseCase) *UserHandler {
 }
 
 // SignUpRequest はユーザー登録のリクエストデータです
-type SignUpRequest struct {
+type signUpRequest struct {
 	Name     string `json:"name"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
 }
 
-type SignUpResponse struct {
+type signUpResponse struct {
 	ID    string `json:"id"`
 	Name  string `json:"name"`
 	Email string `json:"email"`
 }
 
+type SignInRequest struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type signInResponse struct {
+	Token string          `json:"token"`
+	User  *signUpResponse `json:"user"`
+}
+
+// ユーザー登録
 func (h *UserHandler) SignUp(w http.ResponseWriter, r *http.Request) {
-	var req SignUpRequest
+	var req signUpRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		// 一時的に
 		RespondWithError(w, apperror.ErrInvalidJSON)
@@ -48,7 +59,7 @@ func (h *UserHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	res := SignUpResponse{
+	res := signUpResponse{
 		ID:    output.ID,
 		Name:  output.Name,
 		Email: output.Email,
@@ -56,5 +67,37 @@ func (h *UserHandler) SignUp(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-type", "application/json")
 	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(res)
+}
+
+func (h *UserHandler) SignIn(w http.ResponseWriter, r *http.Request) {
+	var req SignInRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		RespondWithError(w, apperror.ErrInvalidJSON)
+		return
+	}
+
+	input := usecase.SignInInput{
+		Email:    req.Email,
+		Password: req.Password,
+	}
+
+	output, err := h.useCase.SignIn(r.Context(), input)
+	if err != nil {
+		RespondWithError(w, err)
+		return
+	}
+
+	res := signInResponse{
+		Token: output.Token,
+		User: &signUpResponse{
+			ID:    output.User.ID,
+			Name:  output.User.Name,
+			Email: output.User.Email,
+		},
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(res)
 }
